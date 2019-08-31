c497509a7f88f03bedf012dcb489c861d518e5b2
relt <haxrelt@gmail.com>
2019-08-31 21:26:07 +0300

    Fix Bug with .lang [part 1]

--- java/com/relt/ToTEN/proxy/CommonProxy.java
+++ java/com/relt/ToTEN/proxy/CommonProxy.java
@@ -1,10 +1,19 @@
 package com.relt.toten.proxy;
 
+import com.relt.toten.init.InitBlocks;
+import com.relt.toten.init.InitItems;
+import com.relt.toten.world.generate.VandiliyGenerator;
+
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 	
 	public void registerItemRenderer(Item item, int meta, String id){}
-	
+	public void init(FMLInitializationEvent e){    
+		GameRegistry.registerWorldGenerator(new VandiliyGenerator(), 0);
+		GameRegistry.addSmelting(new ItemStack(InitBlocks.VANDILIY_ORE), new ItemStack(InitItems.VANDILIY_INGOT), 100);
+	}
 }
--- java/com/relt/ToTEN/world/generate/VandiliyGenerator.java
+++ java/com/relt/ToTEN/world/generate/VandiliyGenerator.java
@@ -1,5 +1,8 @@
 package com.relt.toten.world.generate;
 
+import com.google.common.base.Predicate;
+import com.relt.toten.init.InitBlocks;
+
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.state.pattern.BlockStateMatcher;
 import net.minecraft.init.Blocks;
@@ -12,27 +15,23 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 
 import java.util.Random;
 
-import com.google.common.base.Predicate;
+public class VandiliyGenerator implements IWorldGenerator {
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        if(world.provider.getDimension() == 0)
+            runGenerator(InitBlocks.VANDILIY_ORE.getDefaultState(), 10, 20, 20, 70, BlockStateMatcher.forBlock(Blocks.STONE), world, random, chunkX, chunkZ);
+    }
 
-import com.relt.toten.init.InitBlocks;
-
-public class VandiliyGenerator implements IWorldGenerator{
-
-	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-		if(world.provider.getDimension() == 0)
-			runGenerator(InitBlocks.VANDILIY_ORE.getDefaultState(), 10, 20, 20, 70, BlockStateMatcher.forBlock(Blocks.STONE), world, random, chunkX, chunkZ);
-		
-	}
-	public void runGenerator(IBlockState blockToGen, int blockAmount, int chancesToSpawn, int minHeight, int maxHeight, Predicate<IBlockState> blockToReplace, World world, Random rand, int chunk_X, int chunk_Z){
-		if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
-		return;
-		WorldGenMinable generator = new WorldGenMinable(InitBlocks.VANDILIY_ORE, 5, Blocks.STONE);
-		int heightdiff = 16 - 0 +1;    for (int i = 0; i < 410; i++) {
-			int x = chunk_X * 16 + rand.nextInt(16);        
-			int y = minHeight + rand.nextInt(heightdiff);        
-			int z = chunk_Z * 16 + rand.nextInt(16);
-			generator.generate(world, rand, new BlockPos(x, y, z));    
-		}
-	}
+    public void runGenerator(IBlockState blockToGen, int blockAmount, int chancesToSpawn, int minHeight, int maxHeight, Predicate<IBlockState> blockToReplace, World world, Random rand, int chunk_X, int chunk_Z)
+    {
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight) return;
+        WorldGenMinable generator = new WorldGenMinable(blockToGen, blockAmount, blockToReplace);
+        int heightdiff = maxHeight - minHeight +1;
+        for (int i = 0; i < chancesToSpawn; i++){
+            int x = chunk_X * 16 + rand.nextInt(16);
+            int y = minHeight + rand.nextInt(heightdiff);
+            int z = chunk_Z * 16 + rand.nextInt(16);
+            generator.generate(world, rand, new BlockPos(x, y, z));
+        }
+    }
 }
